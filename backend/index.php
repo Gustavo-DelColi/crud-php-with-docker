37fcaa6d595ca2a0ec10091987130bc9b79364d4
Gustavo-DelColi <gustavodelcoli@hotmail.com>
2024-09-25 13:41:34 -0300

    fix connection db and php

--- backend/index.php
+++ backend/index.php
@@ -2,6 +2,7 @@
 header("Access-Control-Allow-Origin: *");  // Permite todas as origens
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
+header("Content-Type: application/json");
 
 $host = 'postgres-container';
 $db = 'mydatabase';
@@ -9,12 +10,15 @@ $user = 'myuser';
 $pass = 'mypassword';
 $dsn = "pgsql:host=$host;port=5432;dbname=$db;user=$user;password=$pass";
 
+$response = ['message' => 'Hello from the backend'];  // Resposta padrão
+
 try {
     $pdo = new PDO($dsn);
     if ($pdo) {
-        echo "Conexão com o PostgreSQL realizada com sucesso!";
+        $response['db_connection'] = 'Conexão com o PostgreSQL realizada com sucesso!';
     }
 } catch (PDOException $e) {
-    echo $e->getMessage();
+    $response['db_connection_error'] = $e->getMessage();
 }
+echo json_encode($response);
 ?>
